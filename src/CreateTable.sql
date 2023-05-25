4cca96d2172eb62519f29845f3fe6d38f43df284
ks0510 <kshaikh051999@gmail.com>
2023-05-25 18:54:43 +0530

    Added sql file to create employee payroll table

---
+++ src/CreateTable.sql
@@ -0,0 +1,5 @@
+mysql>  create table employee_payroll(id INT AUTO_INCREMENT PRIMARY KEY,name varchar(20),salary DECIMAL(10,2),date DATE);
+Query OK, 0 rows affected (0.14 sec)
+
+mysql> select* FROM employee_payroll;
+Empty set (0.00 sec)
