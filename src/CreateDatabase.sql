a816b7c299433ec3bed75397713daf12fd3c9c7e
ks0510 <kshaikh051999@gmail.com>
2023-05-25 18:42:28 +0530

    Added sql file to create payroll service database

---
+++ src/CreateDatabase.sql
@@ -0,0 +1,20 @@
+mysql> create database payroll_service;
+Query OK, 1 row affected (0.03 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| addressbookservice |
+| employee           |
+| information_schema |
+| mysql              |
+| payroll_service    |
+| performance_schema |
+| sys                |
++--------------------+
+7 rows in set (0.00 sec)
+
+mysql> use payroll_service;
+Database changed
+mysql>
