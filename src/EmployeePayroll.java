6bb8b540f33bf0b2382e9cbbfb376d74eb983163
ks0510 <kshaikh051999@gmail.com>
2023-05-29 18:21:44 +0530

    Added java program to retrive all pay roll data

--- src/EmployeePayroll.java
+++ src/EmployeePayroll.java
@@ -1,13 +1,10 @@
+/*
+ * This java program retrieve all data from employ payroll
+ * 
+ */
 package com.database;
 
-import java.io.Reader;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
+import java.sql.*;
 public class EmployeePayroll {
 
 	public static void main(String[] args) {
@@ -28,23 +25,33 @@ public class EmployeePayroll {
 			try (Statement statement = connection.createStatement()) {
 				String selectQuery = "select * from employee_payroll";
 				ResultSet resultSet = statement.executeQuery(selectQuery);
+				/*
+				 * While loop to iterate each element of database
+				 */
 				while (resultSet.next()) {
 					int id = resultSet.getInt("id");
 					String name = resultSet.getString("name");
 					double salary = resultSet.getInt("salary");
 					Date date = resultSet.getDate("date");
-					Reader gender = resultSet.getCharacterStream("gender");
+					String gender = resultSet.getString("gender");
 					int phone = resultSet.getInt("employee_phone");
 					String address = resultSet.getString("employee_address");
 					String department = resultSet.getString("employee_department");
 					int basic_pay = resultSet.getInt("Basic_Pay");
-				    int deductions = resultSet.getInt("Deductions");
-				    int tax_pay = resultSet.getInt("Taxable_Pay");
-				    int incomeTax = resultSet.getInt("Income_Tax");
-				    int netpay = resultSet.getInt("Net_Pay");
-				    
+					int deductions = resultSet.getInt("Deductions");
+					int tax_pay = resultSet.getInt("Taxable_Pay");
+					int incomeTax = resultSet.getInt("Income_Tax");
+					int netpay = resultSet.getInt("Net_Pay");
+					
+					
+					/*
+					 * To print all data of table
+					 */
 
-					System.out.println("ID :" + id + ",Name : " + name + ", Age: " +salary+date+gender+phone+address+department+basic_pay+deductions+tax_pay+incomeTax+netpay);
+					System.out.println("ID :" + id + ", Name : " + name +", Salary : " + salary + ", Date :" + date
+							+ ", Gender " + gender + ", Phone Number " + phone + ", Employee address " + address
+							+ ", Employee Department " + department + ", Basic Pay " + basic_pay + ", Deductions " + deductions
+							+ ", Tax pay " + tax_pay + ", Income Tax " + incomeTax + ", Net Pay " + netpay);
 				}
 				resultSet.close();
 				System.out.println("Successful");
