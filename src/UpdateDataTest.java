8424d17d4354d876690d2319b723d3f90fb72a9b
ks0510 <kshaikh051999@gmail.com>
2023-06-04 11:29:28 +0530

    Added java program with junit test case

--- src/UpdateDataTest.java
+++ src/UpdateDataTest.java
@@ -5,7 +5,7 @@ package com.database;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
-import junit.framework.Assert;
+
 /**
  * 
  * @author Kaif
@@ -16,10 +16,33 @@ public class UpdateDataTest {
 	
 	UpdateData  update = new UpdateData ();
 	
+	EmployeePayroll emp1 = new EmployeePayroll();
+	
+	AddTableUC8 emp2= new AddTableUC8();
+	
+	AddEmployeeUC11 emp3 =  new AddEmployeeUC11();
+	
 	@Test
 	public void UpadateDataTest() {
 		double salary = 3000000.0;
 		boolean result = update.updateSalary(salary);
-		Assertions.assertTrue(result);
+        Assertions.assertTrue(result);;
+	}
+	
+	@Test
+	public void givenEmployeePayrollInDB_WhenRetrived_shouldMatchCount() {
+		boolean output = emp1.countEntries(12);
+		Assertions.assertTrue(output);
+	}
+	
+	@Test
+	public void givenCreateTableQuery_shouldgiveStatus() {
+		Assertions.assertTrue(emp2.createTable("Table Created"));
+	}
+	
+	@Test
+	public void addedNewEmployee_shouldgiveUpdatedCount() {
+		boolean output = emp3.addEmployee(13);
+		Assertions.assertTrue(output);
 	}
 }
